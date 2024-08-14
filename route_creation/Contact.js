5f01ccde41f97bcbd1e7023db939d78c79583881
SWAPAN KUMAR SHEE <swapankumar.shee2021@uem.edu.in>
2024-08-14 09:35:12 +0530

    Add files via upload

---
+++ route_creation/Contact.js
@@ -0,0 +1,11 @@
+function Contact(){
+    return(
+        <>
+            <div className="contact">
+                <h1>Contact Little Lemon on this page.</h1>
+            </div>
+        </>
+    );
+
+}
+export default Contact;
